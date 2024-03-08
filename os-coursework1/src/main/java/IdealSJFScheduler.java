31b15fe4ed875b35d4b051f77df569e24b42531c
Owen <owenjgibson@gmail.com>
2024-03-08 12:54:04 +0000

    Debugging changes

--- os-coursework1/src/main/java/IdealSJFScheduler.java
+++ os-coursework1/src/main/java/IdealSJFScheduler.java
@@ -30,21 +30,26 @@ public class IdealSJFScheduler extends AbstractScheduler {
    * Returns null if there is no process to run.
    */
   public Process schedule() {
-    if (readyQueue.isEmpty()) {
-      return null;
-    }
 
-    Process shortestProcess = readyQueue.peek();
-    int shortestBurst = shortestProcess.getNextBurst();
-
-    for (Process process : readyQueue) {
-      int nextBurst = process.getNextBurst();
-      if (nextBurst < shortestBurst) {
-        shortestBurst = nextBurst;
-        shortestProcess = process;
+      if (readyQueue.isEmpty()) {
+        return null;
       }
+
+      Process shortestProcess = null;
+      int shortestBurst = Integer.MAX_VALUE;
+
+      System.out.println("Queue before scheduling: ");
+      for (Process process : readyQueue) {
+        int nextBurst = process.getNextBurst();
+        System.out.println("Process ID: " + process.getId() + ", Burst Time: " + process.getNextBurst());
+        if (nextBurst < shortestBurst) {
+          shortestBurst = nextBurst;
+          shortestProcess = process;
+        }
+      }
+      readyQueue.remove(shortestProcess);
+      System.out.println("Scheduler selects process " + shortestProcess);
+      return shortestProcess;
     }
-    System.out.println("Scheduler selects process "+readyQueue.remove(shortestProcess));
-    return shortestProcess;
-  }
+
 }
